f9bc310ac74f6c6ad86b034c9696f87d3920d164
Glitch (aframe) <none>
2022-10-11 10:04:55 +0000

    🎨🔔 Checkpoint ./sidequest-sdk.js:15574369/129 ./index.html:15574369/8

--- sidequest-sdk.js
+++ sidequest-sdk.js
@@ -11,10 +11,13 @@
 });
 
 AFRAME.registerComponent('sq-streetview', {
+   schema: {
+    panoId: {type: 'string', default: 'EusXB0g8G1DOvaPV56X51g'}
+  },
   update: function (oldData) {
     const mesh = this.el.object3D;
     if(mesh){
-       mesh.userData.streetView = this.data; 
+       mesh.userData.streetView = this.data.panoId; 
       console.log(this.el.object3D);
     }
   }
