072f500cf566fffb5d53b090b64f03adbfe31833
Glitch (aframe) <none>
2022-11-16 07:55:48 +0000

    💿🍣 Checkpoint ./sidequest-sdk.js:15574369/4946

--- sidequest-sdk.js
+++ sidequest-sdk.js
@@ -1,50 +1,71 @@
- ['climbable', 'collider', 'sticky', 'slippery', 'lefthand', 'righthand', 'head' ].forEach(d=>{
-            AFRAME.registerComponent('sq-' + d, {
-                update: function (oldData) {
-                    const mesh = this.el.getObject3D('mesh')
-                    if(mesh){
-                        mesh.userData[d] = true;
-                    }
-                }
-            });
-        });
-
-        AFRAME.registerComponent('sq-streetview', {
-            schema: {
-                panoId: {type: 'string', default: 'EusXB0g8G1DOvaPV56X51g'}
-            },
-            update: function (oldData) {
-                const mesh = this.el.object3D;
-                if(mesh){
-                    mesh.userData.streetView = this.data.panoId; 
-                    console.log(this.el.object3D);
-                }
+['climbable', 'collider', 'sticky', 'slippery',].forEach(d => {
+    AFRAME.registerComponent('sq-' + d, {
+        update: function (oldData) {
+            const mesh = this.el.getObject3D('mesh')
+            if(mesh){
+                mesh.userData[d] = true;
             }
-        });
+        }
+    });
+});
 
-        // Deprecated
-        AFRAME.registerComponent('sq-customhome', {
-            schema: {
-                customHome: {type: 'string', default: 'https://cdn.sidequestvr.com/file/167567/canyon_environment.apk'}
-            },
-            update: function (oldData) {
-                const mesh = this.el.object3D;
-                if(mesh){
-                    mesh.userData.customHome = this.data.customHome; 
-                    console.log(this.el.object3D);
-                }
+[ 'lefthand', 'righthand', 'head' ].forEach(d => {
+  AFRAME.registerComponent('sq-' + d, {
+        schema: {
+            positionOffset: {type: 'vec3', default: {x: 0, y: 0, z: 0}},
+            rotationOffset: {type: 'vec3', default: {x: 0, y: 0, z: 0, w: 0}},
+            scaleOffset: {type: 'vec3', default: {x: 1, y: 1, z: 1}},
+        },
+        update: function (oldData) {
+            const mesh = this.el.getObject3D('mesh')
+            if(mesh){
+                mesh.userData[d] = {
+                  enabled: true, 
+                  position: this.data.positionOffset,
+                  rotation: this.data.rotationOffset,
+                  scale: this.data.scaleOffset,
+                };
             }
-        });
+        }
+    });
+})
 
-        AFRAME.registerComponent('sq-questhome', {
-            schema: {
-                url: {type: 'string', default: 'https://cdn.sidequestvr.com/file/167567/canyon_environment.apk'}
-            },
-            update: function (oldData) {
-                const mesh = this.el.object3D;
-                if(mesh){
-                    mesh.userData.customHome = this.data.url; 
-                    console.log(this.el.object3D);
-                }
-            }
-        });
+AFRAME.registerComponent('sq-streetview', {
+    schema: {
+        panoId: {type: 'string', default: 'EusXB0g8G1DOvaPV56X51g'}
+    },
+    update: function (oldData) {
+        const mesh = this.el.object3D;
+        if(mesh){
+            mesh.userData.streetView = this.data.panoId; 
+            console.log(this.el.object3D);
+        }
+    }
+});
+
+// Deprecated
+AFRAME.registerComponent('sq-customhome', {
+    schema: {
+        customHome: {type: 'string', default: 'https://cdn.sidequestvr.com/file/167567/canyon_environment.apk'}
+    },
+    update: function (oldData) {
+        const mesh = this.el.object3D;
+        if(mesh){
+            mesh.userData.customHome = this.data.customHome; 
+            console.log(this.el.object3D);
+        }
+    }
+});
+
+AFRAME.registerComponent('sq-questhome', {
+    schema: {
+        url: {type: 'string', default: 'https://cdn.sidequestvr.com/file/167567/canyon_environment.apk'}
+    },
+    update: function (oldData) {
+        const mesh = this.el.object3D;
+        if(mesh){
+            mesh.userData.customHome = this.data.url; 
+            console.log(this.el.object3D);
+        }
+    }
+});
