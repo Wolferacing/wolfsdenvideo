26f0ab84b6567c053c32676d25627c8386ecb221
Glitch (aframe) <none>
2023-06-30 12:59:30 +0000

    💘💘 Checkpoint ./public/video-player.js:15574369/44 ./public/player.html:15574369/8762

--- public/video-player.js
+++ public/video-player.js
@@ -104,7 +104,7 @@ class VideoSystem {
   playVidya(vidya, currentTrack, currentTime, force) {
     if(this.player) {
       if(this.lastUrl !== this.player.playlist[currentTrack].link || force) {
-        vidya.setAttribute('sq-browser','url: https://sq-synced-videoplayer.glitch.me/player.html?youtube=' + encodeURIComponent(this.player.playlist[currentTrack].link) + '&start=' + currentTime);
+        vidya.setAttribute('sq-browser','url: https://sq-synced-videoplayer.glitch.me/player.html?youtube=' + encodeURIComponent(this.player.playlist[currentTrack].link) + '&start=' + currentTime + '&user=' + window.user.id);
       }
       this.lastUrl = this.player.playlist[currentTrack].link;
     }
