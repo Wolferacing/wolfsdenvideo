0ec6f5d988490ed23041a11ab133b9fd4c08610d
Glitch (aframe) <none>
2023-07-16 09:34:44 +0000

    🌒👰 Checkpoint ./server.ts:15574369/294 ./package.json:15574369/927 ./tsconfig.json:15574369/543 ./server/app.ts:15574369/493

---
+++ server/app.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+    console.log("here1");
+
+export class Server{
+  constructor() {
+    console.log("here2");
+  }
+}
+new Server();
