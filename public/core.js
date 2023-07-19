3cc02e6705a85e97fe34532fb050d3a4bda296e9
Glitch (aframe) <none>
2023-07-19 17:00:15 +0000

    🚥🏡 Checkpoint ./public/core.js:15574369/181

--- public/core.js
+++ public/core.js
@@ -116,11 +116,11 @@ class Core{
       return;
     }
     let button;
-    button = this.setupButton(scene, this.playlistContainer, '0', 'Join In', '1',  'large',  () => {
+    button = this.setupButton(scene, this.playlistContainer, '-1.030', 'join in', '1',  'large',  () => {
       this.imIn = !this.imIn;
-      window.setText(button.object3D.id, this.imIn ? 'Skip It' : 'Join In');
+      window.setText(button.object3D.id, this.imIn ? 'skip it' : 'join in');
       this.sendMessage({ path: this.imIn ? Commands.ADD_TO_PLAYERS : Commands.REMOVE_FROM_PLAYERS });
-    }, 0.4);
+    }, 0);
     
     const yScale = Number(this.params.scale.split(" ")[1]);
      const playlistButton = document.createElement('a-plane');
@@ -138,7 +138,7 @@ class Core{
     playlistButton.addEventListener('click', () => this.openPlaylist());
   }
   setupPlaylistButton(scene, playlistContainer) {
-    this.setupButton(scene, playlistContainer, '-1.5', this.isKaraoke ? 'singers' : 'playlist', '1',  'large',  ()=>{
+    this.setupButton(scene, playlistContainer, '-1.7', this.isKaraoke ? 'singers' : 'playlist', '1',  'large',  ()=>{
       this.openPlaylist();
     })
   }
@@ -146,19 +146,19 @@ class Core{
     window.openPage("https://" + this.hostUrl + "/" + (this.isKaraoke ? 'karaoke' : 'playlist') + "/?instance=" + this.params.instance + ( this.params.playlist ? "&playlist=" + this.params.playlistId : "") + "&user=" + window.user.id +"-_-"+encodeURIComponent(window.user.name));
   }
   setupVolButton(scene, isUp, playlistContainer) {
-    this.setupButton(scene, playlistContainer, isUp ? 1.2 : 1.75, isUp ? '+ vol' : '- vol', '0.5', 'medium', ()=>{
+    this.setupButton(scene, playlistContainer, isUp ? 1.25 : 1.78, isUp ? '+ vol' : '- vol', '0.5', 'medium', ()=>{
         this.setVolume(isUp);
       console.warn({path: Commands.SET_VOLUME, data: this.params.volume});
         this.sendMessage({path: Commands.SET_VOLUME, data: this.params.volume});
     })
   }
   setupSkipButton(scene, isBack, playlistContainer) {
-    this.setupButton(scene, playlistContainer, isBack ? -0.5 : -0.1, isBack ? '<<' : '>>', '0.5',  'small', () => {
+    this.setupButton(scene, playlistContainer, isBack ? -0.475 : -0.125, isBack ? '<<' : '>>', '0.5',  'small', () => {
         this.sendMessage({path: isBack? Commands.SKIP_BACK : Commands.SKIP_FORWARD});
     })
   }
   setupMuteButton(scene, playlistContainer) {
-    this.setupButton(scene, playlistContainer, '0.65', 'mute', '0.5',  'medium', () => {
+    this.setupButton(scene, playlistContainer, '0.73', 'mute', '0.5',  'medium', () => {
       this.params.mute = this.params.mute == 'true' ? 'false' : 'true';
       this.sendMessage({path: Commands.MUTE, data: this.params.mute});
     })
@@ -176,7 +176,7 @@ class Core{
     
     buttonContainer.setAttribute('position', `${xOffset} ${(-yScale*0.335)-(yOffset||0)} 0`);
     const playlistButton = document.createElement('a-entity');
-    playlistButton.setAttribute('sq-boxcollider', `size: 1 0.3 0.05');
+    playlistButton.setAttribute('sq-boxcollider', `size: ${size == 'small' ? '0.3 0.2 0.05': size == 'medium' ? '0.45 0.2 0.05' : '0.6 0.2 0.05' }`);
     playlistButton.setAttribute('sq-interactable', '');
     playlistButton.setAttribute('src', 'https://cdn.glitch.global/cf03534b-1293-4351-8903-ba15ffa931d3/angryimg.png?v=1689619321813');
     
