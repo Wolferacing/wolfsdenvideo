fabc1bfb79ae5ebed84ef3f9946f1c17f5d6793d
Glitch (aframe) <none>
2023-08-21 11:32:57 +0000

    🎉🛍 Checkpoint ./public/portals.js:15574369/8412

---
+++ public/portals.js
@@ -0,0 +1,30 @@
+class Portals {
+  constructor() {
+    this.parseParams();
+  }
+  parseParams(currentScript) {
+    this.currentScript = currentScript;
+    this.setOrDefault("space-limit", "5");
+    this.setOrDefault("space-limit", "5");
+  }
+  setOrDefault(attr, defaultValue) {
+    const value = this.currentScript.getAttribute(attr);
+    this.params = this.params || {};
+    this.params[attr] = value || (this.urlParams.has(attr) ? this.urlParams.get(attr) : defaultValue);
+  }
+}
+(async ()=>{
+  if(window.isBanter) {
+    const now = Date.now();
+    const spaces = await fetch('https://api.sidequestvr.com/v2/communities?is_verified=true&has_space=true&sortOn=user_count,name&descending=true,false&limit=5');
+    
+    const events = await fetch('https://api.sidequestvr.com/v2/events/banter');
+    
+    /*
+        const start = new Date(space.scheduledStartTimestamp);
+        const startTime = start.getTime();
+        const endTime = new Date(space.scheduledEndTimestamp).getTime();
+        const isActive = startTime < Date.now();
+    */
+  }
+})();
