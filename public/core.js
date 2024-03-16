c95f5861477798aa1b4701f33f2d096dc83c1b98
Glitch (aframe) <none>
2024-03-16 23:02:08 +0000

    ⛷🚧 Checkpoint ./public/core.js:15574369/117

--- public/core.js
+++ public/core.js
@@ -432,8 +432,8 @@ class Core{
         break;
       case Commands.RESET_BROWSER:
         if(window.isBanter && this.browser) {
-          console.log("RESET_BROWSER");
-          this.browser.setAttribute("sq-browser", {"url": this.initialUrl});
+          console.log("RESET_BROWSER", {"url": this.initialUrl});
+          this.browser.setAttribute("sq-browser", {"url": this.initialUrl||""});
         }
         break;
       case Commands.STOP:
@@ -445,8 +445,8 @@ class Core{
         this.sendBrowserMessage(json);
       case Commands.SET_BROWSER_URL:
         if(window.isBanter && this.browser) {
-          console.log("SET_BROWSER_URL");
-          this.browser.setAttribute("sq-browser", {"url": json.data.link});
+          console.log("SET_BROWSER_URL", {"url": json.data.link});
+          this.browser.setAttribute("sq-browser", {"url": json.data.link||""});
         }
         break;
       case Commands.CLICK_BROWSER:
